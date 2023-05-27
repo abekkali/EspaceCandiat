ead0bb25a81882c24d34e726e19ad5ecc147d40e
Anass Bekkali <anass.bekkali@uae.ac.ma>
2023-05-27 17:31:50 +0100

    apply candidats include cv and mail confirmation admin page pour gerer les candidatures

---
+++ EspaceCandiat/Controllers/AdminController.cs
@@ -0,0 +1,67 @@
+﻿using EspaceCandiat.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace EspaceCandiat.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> index()
+        {
+            return View(await _context.Candidats.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var candidat = await _context.Candidats.FindAsync(id);
+            if (candidat == null)
+            {
+                return NotFound();
+            }
+
+            _context.Candidats.Remove(candidat);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpGet("/Admin/GetCV/{id}")]
+        public async Task<IActionResult> GetCV(int id)
+        {
+            var candidat = await _context.Candidats.FindAsync(id);
+            if (candidat == null)
+            {
+                return NotFound();
+            }
+
+            var cvPath = candidat.Cv;
+
+            var extension = Path.GetExtension(cvPath).ToLower();
+            var contentType = "application/pdf"; // Par défaut sinon 
+
+            if (extension == ".jpg" || extension == ".jpeg")
+            {
+                contentType = "image/jpeg";
+            }
+            else if (extension == ".png")
+            {
+                contentType = "image/png";
+            }
+
+            return PhysicalFile(cvPath, contentType);
+        }
+
+
+
+    }
+}
---
+++ EspaceCandiat/Services/EmailSender.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net.Mail;
+using System.Net;
+
+namespace EspaceCandiat.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            SmtpClient client = new SmtpClient
+            {
+                Port = 587,
+                Host = "smtp.gmail.com",
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("mettez email", "mdp")// mettez un email valide et mot de passe (SMTP gmail pour ce cas)
+            };
+
+            return client.SendMailAsync("mettez email", email, subject, htmlMessage);
+        }
+
+    }
+}
